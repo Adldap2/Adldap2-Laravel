1e76f8848dd6b5216a32e6f699892a2e2836bef9
Steve Bauman <steven_bauman@outlook.com>
2015-09-09 10:24:13 -0400

    Added more auth tests

--- tests/AdldapTest.php
+++ tests/AdldapTest.php
@@ -25,7 +25,7 @@ class AdldapTest extends FunctionalTestCase
         $this->assertTrue(app()->register('Adldap\Laravel\AdldapAuthServiceProvider'));
     }
 
-    public function testAuth()
+    public function testAuthPasses()
     {
         // Get around E Strict warning about mockery's __call signature being different
         if(defined('E_STRICT')) error_reporting('E_ALL ^ E_STRICT');
@@ -52,11 +52,62 @@ class AdldapTest extends FunctionalTestCase
         Adldap::shouldReceive('users')->once()->andReturn($mockedUsers);
         Adldap::shouldReceive('authenticate')->once()->andReturn(true);
 
-        Auth::attempt(['mail' => 'jdoe@email.com', 'password' => '12345']);
+        $this->assertTrue(Auth::attempt(['mail' => 'jdoe@email.com', 'password' => '12345']));
 
         $user = \DB::table('users')->where('id', '=', 1)->first();
 
         $this->assertEquals('jdoe@email.com', $user->email);
         $this->assertTrue(\Hash::check('12345', $user->password));
+    }
+
+    public function testAuthFails()
+    {
+        // Get around E Strict warning about mockery's __call signature being different
+        if(defined('E_STRICT')) error_reporting('E_ALL ^ E_STRICT');
+
+        $mockedBuilder = Mockery::mock('Adldap\Query\Builder');
+
+        $mockedSearch = Mockery::mock('Adldap\Classes\Search');
+
+        $mockedUsers = Mockery::mock('Adldap\Classes\Users');
+
+        $mockedSearch->shouldReceive('first')->once()->andReturn(null);
+        $mockedSearch->shouldReceive('whereEquals')->once()->andReturn($mockedBuilder);
+
+        $mockedUsers->shouldReceive('search')->once()->andReturn($mockedSearch);
+
+        Adldap::shouldReceive('users')->once()->andReturn($mockedUsers);
+
+        $this->assertFalse(Auth::attempt(['mail' => 'jdoe@email.com', 'password' => '12345']));
+    }
+
+    public function testAuthFailsWhenUserFound()
+    {
+        // Get around E Strict warning about mockery's __call signature being different
+        if(defined('E_STRICT')) error_reporting('E_ALL ^ E_STRICT');
+
+        $mockedBuilder = Mockery::mock('Adldap\Query\Builder');
+
+        $rawAttributes = [
+            'samaccountname' => ['jdoe'],
+            'mail' => ['jdoe@email.com'],
+            'cn' => ['John Doe'],
+        ];
+
+        $adUser = (new User([], $mockedBuilder))->setRawAttributes($rawAttributes);
+
+        $mockedSearch = Mockery::mock('Adldap\Classes\Search');
+
+        $mockedUsers = Mockery::mock('Adldap\Classes\Users');
+
+        $mockedSearch->shouldReceive('first')->once()->andReturn($adUser);
+        $mockedSearch->shouldReceive('whereEquals')->once()->andReturn($mockedBuilder);
+
+        $mockedUsers->shouldReceive('search')->once()->andReturn($mockedSearch);
+
+        Adldap::shouldReceive('users')->once()->andReturn($mockedUsers);
+        Adldap::shouldReceive('authenticate')->once()->andReturn(false);
+
+        $this->assertFalse(Auth::attempt(['mail' => 'jdoe@email.com', 'password' => '12345']));
     }
 }
