8b635d494490151ae68aa9d358db7f596f25d3f9
Steve Bauman <steven_bauman@outlook.com>
2015-09-09 10:30:39 -0400

    Remove migration namespace

--- tests/stubs/migrations/2014_10_12_000000_create_users_table.php
+++ tests/stubs/migrations/2014_10_12_000000_create_users_table.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Adldap\Laravel\Tests\Stubs\Migrations;
-
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
