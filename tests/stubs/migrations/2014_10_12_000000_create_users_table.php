1606ef9c63399f664cb18789a0da87a224485a12
Steve Bauman <steven_bauman@outlook.com>
2015-09-09 10:43:03 -0400

    Try to fix tests for travis

--- tests/stubs/migrations/2014_10_12_000000_create_users_table.php
+++ tests/stubs/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Adldap\Laravel\Tests\Stubs\Migrations;
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
