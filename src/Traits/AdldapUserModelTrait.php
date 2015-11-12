06f985e308a8da806951590859361c2596c933d7
Steve Bauman <steven_bauman@outlook.com>
2015-11-12 16:19:14 -0500

    Update variable type

--- src/Traits/AdldapUserModelTrait.php
+++ src/Traits/AdldapUserModelTrait.php
@@ -7,7 +7,7 @@ trait AdldapUserModelTrait
     /**
      * The Adldap User that is bound to the current model.
      *
-     * @var \Adldap\Models\User
+     * @var null|\Adldap\Models\User
      */
     public $adldapUser;
 }
