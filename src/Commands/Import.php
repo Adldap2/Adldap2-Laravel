11796f1accd759064ac6d60a795b42c421a65a2b
Steve Bauman <steven_bauman@outlook.com>
2016-06-29 11:35:22 -0400

    Sort use.

--- src/Commands/Import.php
+++ src/Commands/Import.php
@@ -3,9 +3,9 @@
 namespace Adldap\Laravel\Commands;
 
 use Adldap\Laravel\Traits\ImportsUsers;
-use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Console\Command;
 
 class Import extends Command
 {
