9e4c003008da24c9ea3f9a13a376304aeaa43b1a
Steve Bauman <steven_bauman@outlook.com>
2017-01-25 17:04:59 -0500

    Renamed HasAdldapUser trait to HasLdapUser

---
+++ src/Traits/HasLdapUser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Adldap\Laravel\Traits;
+
+use Adldap\Models\User;
+
+trait HasLdapUser
+{
+    /**
+     * The LDAP User that is bound to the current model.
+     *
+     * @var null|\Adldap\Models\User
+     */
+    public $ldap;
+
+    /**
+     * Sets the LDAP user that is bound to the model.
+     *
+     * @param User $user
+     */
+    public function setLdapUser(User $user)
+    {
+        $this->ldap = $user;
+    }
+}
