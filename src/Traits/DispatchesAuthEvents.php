706773e6b70f858a3596ef5744a0c5f5e7eaadd0
Steve Bauman <steven_bauman@outlook.com>
2017-02-06 17:41:30 -0500

    Added trait for firing auth events

---
+++ src/Traits/DispatchesAuthEvents.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Adldap\Laravel\Traits;
+
+use Adldap\Models\User;
+use Adldap\Laravel\Events\AuthenticatedWithWindows;
+use Adldap\Laravel\Events\DiscoveredWithCredentials;
+use Adldap\Laravel\Events\AuthenticatedWithCredentials;
+use Illuminate\Contracts\Auth\Authenticatable;
+
+trait DispatchesAuthEvents
+{
+    /**
+     * Dispatches an authentication event when users login by their credentials.
+     *
+     * @param User            $user
+     * @param Authenticatable $model
+     *
+     * @return void
+     */
+    public function handleAuthenticatedWithCredentials(User $user, Authenticatable $model)
+    {
+        event(new AuthenticatedWithCredentials($user, $model));
+    }
+
+    /**
+     * Dispatches an event when a user has logged in via the WindowsAuthenticate middleware.
+     *
+     * @param User                 $user
+     * @param Authenticatable|null $model
+     *
+     * @return void
+     */
+    protected function handleAuthenticatedWithWindows(User $user, Authenticatable $model = null)
+    {
+        event(new AuthenticatedWithWindows($user, $model));
+    }
+
+    /**
+     * Dispatches an event when a user has been discovered by the their credentials.
+     *
+     * @param User $user
+     */
+    protected function handleDiscoveredWithCredentials(User $user)
+    {
+        event(new DiscoveredWithCredentials($user));
+    }
+}
