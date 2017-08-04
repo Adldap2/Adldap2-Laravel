20401b6b4648eb72090612e84c37dfdbcb29842f
Steve Bauman <steven_bauman@outlook.com>
2017-08-04 17:05:39 -0400

    Small refactor

--- src/Validation/Rules/DenyTrashed.php
+++ src/Validation/Rules/DenyTrashed.php
@@ -2,6 +2,7 @@
 
 namespace Adldap\Laravel\Validation\Rules;
 
+use Illuminate\Support\Facades\Event;
 use Adldap\Laravel\Events\AuthenticatedModelTrashed;
 
 class DenyTrashed extends Rule
@@ -11,12 +12,24 @@ class DenyTrashed extends Rule
      */
     public function isValid()
     {
-        if (method_exists($this->model, 'trashed') && $this->model->trashed()) {
-            event(new AuthenticatedModelTrashed($this->user, $this->model));
+        if ($this->isTrashed()) {
+            Event::fire(
+                new AuthenticatedModelTrashed($this->user, $this->model)
+            );
 
             return false;
         }
 
         return true;
+    }
+
+    /**
+     * Determines if the current model is trashed.
+     *
+     * @return bool
+     */
+    protected function isTrashed()
+    {
+        return method_exists($this->model, 'trashed') && $this->model->trashed();
     }
 }
