04ea14c694546f8526f9a95d14bb4c7fd489ee92
jonathangoncalves <jonathangoncalves@users.noreply.github.com>
2018-09-17 19:08:50 -0300

    Adding PHPDoc Blocks with mixin
    This will help with documentation and IDE inspections.

--- src/Facades/Adldap.php
+++ src/Facades/Adldap.php
@@ -4,6 +4,11 @@ namespace Adldap\Laravel\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Adldap
+ * @package Adldap\Laravel\Facades
+ * @mixin \Adldap\Connections\Provider
+ */
 class Adldap extends Facade
 {
     /**
