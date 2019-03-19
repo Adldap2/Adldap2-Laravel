85c56ca6e30dfedf2c01cbd9792630c7bc7b91c2
Steve Bauman <steven_bauman@outlook.com>
2019-03-18 21:55:40 -0400

    Set logging to false by default

--- src/Config/config.php
+++ src/Config/config.php
@@ -10,9 +10,13 @@ return [
     | This option enables logging all LDAP operations on all configured
     | connections such as bind requests and CRUD operations.
     |
+    | Log entries will be created in your default logging stack.
+    |
+    | This option is extremely helpful for debugging connectivity issues.
+    |
     */
 
-    'logging' => env('LDAP_LOGGING', true),
+    'logging' => env('LDAP_LOGGING', false),
 
     /*
     |--------------------------------------------------------------------------
