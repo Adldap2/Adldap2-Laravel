d72e7a0bf57c4760378d485e3ea0f4d203317feb
Steve Bauman <steven_bauman@outlook.com>
2019-03-19 11:41:51 -0400

    Added static method definitions to Resolver facade

--- src/Facades/Resolver.php
+++ src/Facades/Resolver.php
@@ -5,6 +5,20 @@ namespace Adldap\Laravel\Facades;
 use Illuminate\Support\Facades\Facade;
 use Adldap\Laravel\Resolvers\ResolverInterface;
 
+/**
+ * @method static void setConnection(string $connection)
+ * @method static \Adldap\Models\Model|null byId(string $identifier)
+ * @method static \Adldap\Models\User|null byCredentials(array $credentials)
+ * @method static \Adldap\Models\User|null byModel(\Illuminate\Contracts\Auth\Authenticatable $model)
+ * @method static boolean authenticate(\Adldap\Models\User $user, array $credentials = [])
+ * @method static \Adldap\Query\Builder query()
+ * @method static string getLdapDiscoveryAttribute()
+ * @method static string getLdapAuthAttribute()
+ * @method static string getDatabaseUsernameColumn()
+ * @method static string getDatabaseIdColumn()
+ *
+ * @see \Adldap\Laravel\Resolvers\UserResolver
+ */
 class Resolver extends Facade
 {
     /**
