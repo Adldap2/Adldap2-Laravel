e7e60f0f10ff845bf8e8c965258c86a33c9ab266
Steve Bauman <steven_bauman@outlook.com>
2019-06-28 13:34:46 -0400

    Assert that rules are executed when authenticating with SSO middleware
    #748

--- tests/WindowsAuthenticateTest.php
+++ tests/WindowsAuthenticateTest.php
@@ -5,6 +5,8 @@ namespace Adldap\Laravel\Tests;
 use Mockery as m;
 use Adldap\Query\Builder;
 use Adldap\Laravel\Facades\Resolver;
+use Adldap\Laravel\Tests\Models\TestUser;
+use Adldap\Laravel\Validation\Rules\DenyTrashed;
 use Adldap\Laravel\Middleware\WindowsAuthenticate;
 
 class WindowsAuthenticateTest extends DatabaseTestCase
@@ -15,8 +17,6 @@ class WindowsAuthenticateTest extends DatabaseTestCase
         $request = app('request');
 
         $request->server->set('AUTH_USER', 'jdoe');
-
-        $middleware = app(WindowsAuthenticate::class);
 
         $user = $this->makeLdapUser([
             'objectguid'        => ['cc07cacc-5d9d-fa40-a9fb-3a4d50a172b0'],
@@ -35,10 +35,9 @@ class WindowsAuthenticateTest extends DatabaseTestCase
             ->shouldReceive('getDatabaseIdColumn')->twice()->andReturn('objectguid')
             ->shouldReceive('getDatabaseUsernameColumn')->once()->andReturn('email')
             ->shouldReceive('getLdapDiscoveryAttribute')->once()->andReturn('userprincipalname')
-            ->shouldReceive('byModel')->once()->andReturn(($user));
+            ->shouldReceive('byModel')->once()->andReturn($user);
 
-        $middleware->handle($request, function () {
-        });
+        app(WindowsAuthenticate::class)->handle($request, function () {});
 
         $authenticated = auth()->user();
 
@@ -55,8 +54,6 @@ class WindowsAuthenticateTest extends DatabaseTestCase
 
         $request->server->set('AUTH_USER', 'jdoe');
 
-        $middleware = app(WindowsAuthenticate::class);
-
         $query = m::mock(Builder::class);
 
         $query
@@ -65,8 +62,50 @@ class WindowsAuthenticateTest extends DatabaseTestCase
 
         Resolver::shouldReceive('query')->once()->andReturn($query);
 
-        $middleware->handle($request, function () {
+        app(WindowsAuthenticate::class)->handle($request, function () {});
+
+        $this->assertNull(auth()->user());
+    }
+
+    /** @test */
+    public function middleware_validates_authenticating_users()
+    {
+        // Deny deleted users from authenticating.
+        config()->set('ldap_auth.rules', [DenyTrashed::class]);
+
+        // Create the deleted user.
+        tap(new TestUser(), function ($user) {
+            $user->name = 'John Doe';
+            $user->email = 'jdoe@email.com';
+            $user->password = 'secret';
+            $user->deleted_at = now();
+
+            $user->save();
         });
+
+        $request = app('request');
+
+        $request->server->set('AUTH_USER', 'jdoe');
+
+        $user = $this->makeLdapUser([
+            'objectguid'        => ['cc07cacc-5d9d-fa40-a9fb-3a4d50a172b0'],
+            'cn'                => ['John Doe'],
+            'userprincipalname' => ['jdoe@email.com'],
+            'samaccountname'    => ['jdoe'],
+        ]);
+
+        $query = m::mock(Builder::class);
+
+        $query
+            ->shouldReceive('whereEquals')->once()->withArgs(['samaccountname', 'jdoe'])->andReturn($query)
+            ->shouldReceive('first')->once()->andReturn($user);
+
+        Resolver::shouldReceive('query')->once()->andReturn($query)
+            ->shouldReceive('getDatabaseIdColumn')->twice()->andReturn('objectguid')
+            ->shouldReceive('getDatabaseUsernameColumn')->once()->andReturn('email')
+            ->shouldReceive('getLdapDiscoveryAttribute')->once()->andReturn('userprincipalname');
+
+        app(WindowsAuthenticate::class)->handle($request, function () {});
 
         $this->assertNull(auth()->user());
     }
