2ac03082c360c6a49ad91957d2253e0556d4c877
Steve Bauman <steven_bauman@outlook.com>
2019-12-13 07:31:12 -0500

    Fix test

--- tests/Console/ImportTest.php
+++ tests/Console/ImportTest.php
@@ -166,9 +166,6 @@ class ImportTest extends DatabaseTestCase
         $this->assertTrue($model->fresh()->trashed());
     }
 
-    /**
-     * @expectedException \RuntimeException
-     */
     public function test_filter_option_applies_to_ldap_query()
     {
         $f = '(samaccountname=jdoe)';
@@ -182,6 +179,7 @@ class ImportTest extends DatabaseTestCase
 
         Resolver::shouldReceive('query')->once()->andReturn($b);
 
-        $this->artisan('adldap:import', ['--filter' => $f, '--no-interaction' => true]);
+        $this->artisan('adldap:import', ['--filter' => $f, '--no-interaction' => true])
+            ->expectsOutput('There were no users found to import.');
     }
 }
